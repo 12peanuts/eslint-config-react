4acb54b77affc4d75113d3ea4d64f9f0e8afa022
hx2ryu <hx2ryu@gmail.com>
2023-03-31 17:42:32 +0900

    chore: add rules

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-module.export = {
+module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
     "airbnb",
+    "airbnb/hooks",
     "airbnb-typescript",
   ],
-  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+    project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
-  rules: {},
-  settings: {
-    react: {
-      version: "detect",
-    },
+  rules: {
+    "arrow-body-style": 0,
+    "react/jsx-indent": 0,
+    "react/jsx-indent-props": 0,
+    "react/function-component-definition": 0,
+    "react/require-default-props": 0,
+    "react/jsx-props-no-spreading": 0,
+    "import/prefer-default-export": 0,
+    "@typescript-eslint/indent": 0,
+    "@typescript-eslint/no-unused-vars": 1,
+    "@typescript-eslint/no-unsafe-assignment": 0,
+    "object-curly-newline": 0,
+    "prettier/prettier": 0,
+    "import/no-cycle": 0,
+    "import/order": 0,
   },
+  plugins: ["@typescript-eslint"],
 };
